0af422cb3dd05cfbcd73cff46f05b0912c0fe3de
Airon Herbert <aironherbertms@gmail.com>
2022-04-24 17:28:24 -0300

    Remove unused component

--- src/components/add-todo.js
+++ src/components/add-todo.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import TodoDataService from '../services/todos';
-import { Link } from 'react-router-dom';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
